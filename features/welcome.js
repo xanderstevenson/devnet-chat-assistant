65811d6974277c01b315300b49c3ec814ba52e95
Alexander W. Stevenson <27918923+xanderstevenson@users.noreply.github.com>
2020-03-20 16:11:07 -0500

    Update welcome.js
    Reinserting the good welcome jumbotron from the previous incarnation of this bot

--- features/welcome.js
+++ features/welcome.js
@@ -1,20 +1,27 @@
-//
 // Welcome message 
 // sent as the bot is added to a Room
 //
 module.exports = function (controller) {
 
-    controller.on( 'memberships.created', async ( bot, message ) => {
+    controller.on( 'memberships.created', async( bot, message ) => {
 
-        let markDown = `Hi, I am the **${ controller.adapter.identity.displayName }** bot!  \n`
-        markDown += 'Type `help` to learn more about my skills.  ';
+        let markDown = `#### **Welcome to DevNet!\
+                        \n\n![alt text](https://github.com/xanderstevenson/botkit-template/blob/master/assets/images/CiscoDevNetSmall.png)
+                        \n\n _**Self-Help**_ options for the _**DevNet Associate Fundamentals Course**_ (blue text = clickable link):\
+                        \n --- \
+                        \n * **Search for common issues and their answers** in the course <a href='https://developer.cisco.com/docs/fundamentals/help/'>**FAQs**<a>. \
+                        \n * **Questions on content** or **help with the course labs** can be found at the <a href='https://learningnetwork.cisco.com/s/topic/0TO3i0000008jY5GAI/devnet-certifications-community'>**DevNet Certifications Community**</a>.\
+                        \n * **Technical issues** with your course, including page appearance, video or lab functionality, or payments? \
+                        <a href='https://devnetsupport.cisco.com/hc/en-us/requests/new?ticket_form_id=360002862214'>**Open a ticket**</a> with DevNet Course Support. \
+                        \n`;
+
 
         if ( message.data.roomType == 'group' ) {
 
-            markDown += `\n_Note that this is a "group" space.\n  I will answer only if mentioned!  \n`
-            markDown += `For help, enter: ${ controller.checkAddMention( message.data.roomType, 'help' ) }_`
+            markDown += `\n<u>Note</u>: this is a "group" space and I (the Bot) will answer only if mentioned!  \n`
+            markDown += `For help, enter: ${ controller.checkAddMention( message.data.roomType, '' ) } help`
         }
-
+//         console.log('memberships created', message);
         await bot.reply( message, { markdown : markDown} );
     });
 }
