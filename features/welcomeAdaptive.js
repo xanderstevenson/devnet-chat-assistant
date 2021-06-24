e927a15586e11a61ac1eb99b1f9431e88a05af71
Alexander Stevenson (alexstev) <alexstev@cisco.com>
2021-06-24 13:05:25 -0400

    oh Lord

--- features/welcomeAdaptive.js
+++ features/welcomeAdaptive.js
@@ -2,24 +2,25 @@ var AdaptiveCard = require('/assets/cards/welcomeCard.json');
 
 module.exports = function (controller) {
 
-    controller.botCommons = {
+    // controller.botCommons = {
 
-        healthCheck: process.env.PUBLIC_ADDRESS,
-        upSince: new Date( Date.now() ).toGMTString(),
-        botName: controller.adapter.identity.displayName,
-        botVersion: 'v' + require( '../package.json' ).version,
-        owner: process.env.OWNER,
-        support: process.env.SUPPORT,
-        botkitVersion: controller.version,
-        platform: process.env.PLATFORM,
-        code: process.env.CODE
-    }
+    //     healthCheck: process.env.PUBLIC_ADDRESS,
+    //     upSince: new Date( Date.now() ).toGMTString(),
+    //     botName: controller.adapter.identity.displayName,
+    //     botVersion: 'v' + require( '../package.json' ).version,
+    //     owner: process.env.OWNER,
+    //     support: process.env.SUPPORT,
+    //     botkitVersion: controller.version,
+    //     platform: process.env.PLATFORM,
+    //     code: process.env.CODE
+    // }
 
     controller.hears( 'what?', 'message,direct_message', async ( bot, message ) => {
 
                 // Author a card
         // In practice you'll probably get this from a service
         // see http://adaptivecards.io/samples/ for inspiration
+
         var card = {
             "type": "AdaptiveCard",
             "version": "1.0",
@@ -46,7 +47,8 @@ module.exports = function (controller) {
                 }
             ]
         };
-        await bot.reply( message, { "attachment": card } );
+
+        await bot.reply( message, { markdown: 'test' }, { attachment: card } );
     });
 
     controller.commandHelp.push( { command: 'what', text: 'card testing' } );
