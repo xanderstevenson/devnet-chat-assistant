4d2f57b2f0df398bfd113e22c924200255cc331f
Alexander Stevenson (alexstev) <alexstev@cisco.com>
2021-06-24 13:27:09 -0400

    yolo3

--- features/welcomeAdaptive.js
+++ features/welcomeAdaptive.js
@@ -21,35 +21,36 @@ module.exports = function (controller) {
         // In practice you'll probably get this from a service
         // see http://adaptivecards.io/samples/ for inspiration
 
-        // var card = {
-        //     "type": "AdaptiveCard",
-        //     "version": "1.0",
-        //     "body": [
-        //         {
-        //             "type": "Image",
-        //             "url": "http://adaptivecards.io/content/adaptive-card-50.png"
-        //         },
-        //         {
-        //             "type": "TextBlock",
-        //             "text": "Hello **Adaptive Cards!**"
-        //         }
-        //     ],
-        //     "actions": [
-        //         {
-        //             "type": "Action.OpenUrl",
-        //             "title": "Learn more",
-        //             "url": "http://adaptivecards.io"
-        //         },
-        //         {
-        //             "type": "Action.OpenUrl",
-        //             "title": "GitHub",
-        //             "url": "http://github.com/Microsoft/AdaptiveCards"
-        //         }
-        //     ]
-        // };
+        var card = {
+            "type": "AdaptiveCard",
+            "version": "1.0",
+            "body": [
+                {
+                    "type": "Image",
+                    "url": "http://adaptivecards.io/content/adaptive-card-50.png"
+                },
+                {
+                    "type": "TextBlock",
+                    "text": "Hello **Adaptive Cards!**"
+                }
+            ],
+            "actions": [
+                {
+                    "type": "Action.OpenUrl",
+                    "title": "Learn more",
+                    "url": "http://adaptivecards.io"
+                },
+                {
+                    "type": "Action.OpenUrl",
+                    "title": "GitHub",
+                    "url": "http://github.com/Microsoft/AdaptiveCards"
+                }
+            ]
+        };
 
         await bot.reply( message, 'Yolo' );
         await bot.reply( message, { markdown: 'test' } );
+        await bot.reply( message, { attachment: card } );
     });
 
     controller.commandHelp.push( { command: 'what', text: 'card testing' } );
